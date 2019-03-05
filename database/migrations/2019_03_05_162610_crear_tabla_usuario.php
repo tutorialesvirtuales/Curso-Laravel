d2aa90661a538028e5002f541473e3c1227b50e7
tutorialesvirtuales <luzarami@gmail.com>
2019-03-05 11:37:59 -0500

    Trabajo Con Migraciones

--- database/migrations/2019_03_05_162610_crear_tabla_usuario.php
+++ database/migrations/2019_03_05_162610_crear_tabla_usuario.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CrearTablaUsuario extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('usuario', 50);
+            $table->string('password', 100);
+            $table->string('nombre', 50);
+            $table->timestamps();
         });
     }
 
@@ -27,6 +29,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('usuario');
     }
 }
